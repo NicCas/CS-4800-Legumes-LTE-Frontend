015763ffb0e9188de151d3ca177144e2c7ef4abc
ulturac <ilovemyramen@gmail.com>
2021-04-16 14:38:27 -0700

    I added axios to StoreHome.js

--- src/stores/StoreHome.js
+++ src/stores/StoreHome.js
@@ -1,54 +1,76 @@
 import React from "react";
+import axios from "axios";
 import "./StoreHome.css";
 import owner from "../assets/owner_placeholder.png";
-import { Link } from "react-router-dom";
+//axios.defaults.withCredentials = true;
 
-function StoreHome() {
-  return (
-    <div id="store-home">
-      <div id="header">
-        <h1>Stores</h1>
-        Meet the owners and learn more about our wonderful selection of local
-        stores!
-      </div>
-      <div class="list-group">
-        <Link to="storepage/1" class="nav-link">
-          <button
-            type="submit"
-            class="list-group-item list-group-item-action"
-            name="store_name"
-          >
-            <div class="row">
-              <div class="column left">
-                <t>
-                  Store Name
-                  <br />
-                  <b>Rating: </b> #.#
-                </t>
-              </div>
+export default class StoreHome extends React.Component {
 
-              <div class="column middle">
-                <img
-                  id="owner-icon"
-                  src={owner}
-                  alt="Placeholder Owner Picture"
-                  //style="width:60px;height:60px;"
-                ></img>
+  state = {
+    stores: []
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://chickpeaapi.glitch.me/stores`)
+      .then((res) => {
+        const data = res.data;
+        this.setState({ stores: data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  render(){
+    return (
+      <div id="store-home">
+        <div id="header">
+          <h1>Stores</h1>
+          Meet the owners and learn more about our wonderful selection of local
+          stores!
+        </div>
+        <div class="list-group">
+        { this.state.stores.map(
+            store =>
+            <form action="/stores/detail" method="post">
+            <button
+              type="submit"
+              class="list-group-item list-group-item-action"
+              name="store_name"
+            >
+              <div class="row">
+                <div class="column left">
+                  <t>
+                    {store.Store_Name}
+                    <br />
+                    <b>Rating: </b> {store.Rating}
+                  </t>
+                </div>
+  
+                <div class="column middle">
+                  <img
+                    id="owner-icon"
+                    src={owner}
+                    alt="Placeholder Owner Picture"
+                    //style="width:60px;height:60px;"
+                  ></img>
+                </div>
+                <div class="column right">
+                  <t>
+                  {store.Description}
+                  </t>
+                </div>
               </div>
-              <div class="column right">
-                <t>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </t>
-              </div>
-            </div>
-          </button>
-        </Link>
+  
+              <input type="hidden" name="store_id" />
+            </button>
+          </form>
+          ) }
+          <div id="extra-space-small"></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default StoreHome;
