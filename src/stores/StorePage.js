87393b26cfb22588067cd64e7a6b8beda6304545
seaungst <selena.elise03@gmail.com>
2021-04-19 16:44:42 -0700

    changes to storepage for rating system

--- src/stores/StorePage.js
+++ src/stores/StorePage.js
@@ -13,7 +13,7 @@ export default class StorePage extends React.Component {
     super(props);
     this.state = {
       items: {},
-      store: {}
+      store: {},
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
@@ -25,13 +25,12 @@ export default class StorePage extends React.Component {
     axios("https://chickpeaapi.glitch.me/stores/detail", {
       method: "post",
       data: {
-        store_id: currentComponent.props.match.params.store_id
-      }
-    }).then(function(response) {
+        store_id: currentComponent.props.match.params.store_id,
+      },
+    }).then(function (response) {
       const store_data = response.data.store;
       const item_list = response.data.items;
-      currentComponent.setState({items: item_list,
-                    store: store_data});
+      currentComponent.setState({ items: item_list, store: store_data });
     });
     // grabbing url parameter /:store_id
     //console.log(this.props.match.params);
@@ -54,23 +53,61 @@ export default class StorePage extends React.Component {
                 <h2>{this.state.store.Store_Name}</h2>
 
                 <div class="rating">
-                  <input type="radio" name="rating" value="5" id="5"></input>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value="5"
+                    id="5"
+                    disabled="true"
+                  ></input>
                   <label for="5">☆</label>
-                  <input type="radio" name="rating" value="4" id="4"></input>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value="4"
+                    id="4"
+                    checked=""
+                    disabled="true"
+                  ></input>
                   <label for="4">☆</label>
-                  <input type="radio" name="rating" value="3" id="3"></input>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value="3"
+                    id="3"
+                    checked=""
+                    disabled="true"
+                  ></input>
                   <label for="3">☆</label>
-                  <input type="radio" name="rating" value="2" id="2"></input>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value="2"
+                    id="2"
+                    checked=""
+                    disabled="true"
+                  ></input>
                   <label for="2">☆</label>
-                  <input type="radio" name="rating" value="1" id="1"></input>
+                  <input
+                    type="radio"
+                    name="rating"
+                    value="1"
+                    id="1"
+                    checked="checked"
+                    disabled="false"
+                  ></input>
                   <label for="1">☆</label>
                 </div>
 
                 <p>
-                  {this.state.store.Address_Street}<br></br>
-                  {this.state.store.Address_City}<br></br>
-                  {this.state.store.Address_State}<br></br>
-                  {this.state.store.Address_Zipcode}<br></br>
+                  {this.state.store.Address_Street}
+                  <br></br>
+                  {this.state.store.Address_City}
+                  <br></br>
+                  {this.state.store.Address_State}
+                  <br></br>
+                  {this.state.store.Address_Zipcode}
+                  <br></br>
                   {this.state.store.Phone_Number}
                 </p>
 
