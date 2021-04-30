e341e3b93dbb842751b3751211d7854c0e3bfe22
Ryan <ryan.atienza97@gmail.com>
2021-04-29 19:32:32 -0700

    store info in new stores page populates

--- src/stores/StorePage.js
+++ src/stores/StorePage.js
@@ -4,6 +4,7 @@ import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
 import "./StorePage.css";
 import item from "../assets/item_placeholder.png";
+import axios from 'axios'
 
 {/* For dynamic implementation ItemCards code should be placed back into the main function, to simplify passing arguments.
   * it is seperated here for ease of testing */}
@@ -44,167 +45,200 @@ function ItemCards() {
 }
 
 {/* The main funcion */}
-function Store() {
-  return (
-    <div id="store-page">
-      <div class="store-grid-container">
-        {/* Container for the first two boxes; store info and the category buttons */}
-        <div id="top-items">
-          {/* Store Info box */}
-          <div id="store-info" class="store-grid-item">
-            <div id="backsplash-div"></div>
-            <h2>state.store.Store_Name</h2>
 
-            {/* Code for the star rating */}
-            <div class="info-block" id="info-title">
-              <div id="rating-wrapper">
-                <div class="rating">
-                  <input
-                    type="radio"
-                    name="rating"
-                    value="5"
-                    id="5"
-                    disabled="true"
-                  ></input>
-                  <label for="5">☆</label>
-                  <input
-                    type="radio"
-                    name="rating"
-                    value="4"
-                    id="4"
-                    checked=""
-                    disabled="true"
-                  ></input>
-                  <label for="4">☆</label>
-                  <input
-                    type="radio"
-                    name="rating"
-                    value="3"
-                    id="3"
-                    checked="checked"
-                    disabled="true"
-                  ></input>
-                  <label for="3">☆</label>
-                  <input
-                    type="radio"
-                    name="rating"
-                    value="2"
-                    id="2"
-                    checked=""
-                    disabled="true"
-                  ></input>
-                  <label for="2">☆</label>
-                  <input
-                    type="radio"
-                    name="rating"
-                    value="1"
-                    id="1"
-                    checked=""
-                    disabled="true"
-                  ></input>
-                  <label for="1">☆</label>
+export default class Store extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      store: {}
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount() {
+    let currentComponent = this;
+    // axios request will go in here
+    axios("https://chickpeaapi.glitch.me/stores/detail", {
+      method: "post",
+      data: {
+        store_id: currentComponent.props.match.params.store_id
+      }
+    }).then(function(response) {
+      const store_data = response.data.store;
+      const item_list = response.data.items;
+      currentComponent.setState({
+        items: item_list,
+        store: store_data
+        });
+    });
+    // grabbing url parameter /:store_id
+    //console.log(this.props.match.params);
+  }
+
+  render() {
+    return (
+      <div id="store-page">
+        <div class="store-grid-container">
+          {/* Container for the first two boxes; store info and the category buttons */}
+          <div id="top-items">
+            {/* Store Info box */}
+            <div id="store-info" class="store-grid-item">
+              <div id="backsplash-div"></div>
+              <h2>{this.state.store.Store_Name}</h2>
+  
+              {/* Code for the star rating */}
+              <div class="info-block" id="info-title">
+                <div id="rating-wrapper">
+                  <div class="rating">
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="5"
+                      id="5"
+                      disabled="true"
+                    ></input>
+                    <label for="5">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="4"
+                      id="4"
+                      checked=""
+                      disabled="true"
+                    ></input>
+                    <label for="4">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="3"
+                      id="3"
+                      checked="checked"
+                      disabled="true"
+                    ></input>
+                    <label for="3">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="2"
+                      id="2"
+                      checked=""
+                      disabled="true"
+                    ></input>
+                    <label for="2">☆</label>
+                    <input
+                      type="radio"
+                      name="rating"
+                      value="1"
+                      id="1"
+                      checked=""
+                      disabled="true"
+                    ></input>
+                    <label for="1">☆</label>
+                  </div>
+                </div>
+                <div id="store-info-text">
+                  <p>
+                    {this.state.store.Address_Street}
+                    <br></br>
+                    {this.state.store.Address_City}
+                    <br></br>
+                    {this.state.store.Address_State}
+                    <br></br>
+                    {this.state.store.Address_Zipcode}
+                    <br></br>
+                    {this.state.store.Phone_Number}
+                  </p>
                 </div>
               </div>
-              <div id="store-info-text">
-                <p>
-                  state.store.Address_Street
-                  <br></br>
-                  state.store.Address_City
-                  <br></br>
-                  state.store.Address_State
-                  <br></br>
-                  state.store.Address_Zipcode
-                  <br></br>
-                  state.store.Phone_Number
-                </p>
+            </div>
+  
+            {/* Categories button box */}
+            <div class="info-block store-grid-item" id="categories">
+              <h3>Categories</h3>
+              {/* This section should be a dynamic loop */}
+              <div id="categories-grid">
+                {/* href needs to be dynamically named, maybe the category title itself?*/}
+                <a class="cat" href="#cat1">
+                  Category
+                </a>
+                <a class="cat" href="#cat2">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
+                <a class="cat" href="#">
+                  Category
+                </a>
               </div>
             </div>
           </div>
-
-          {/* Categories button box */}
-          <div class="info-block store-grid-item" id="categories">
-            <h3>Categories</h3>
-            {/* This section should be a dynamic loop */}
-            <div id="categories-grid">
-              {/* href needs to be dynamically named, maybe the category title itself?*/}
-              <a class="cat" href="#cat1">
-                Category
-              </a>
-              <a class="cat" href="#cat2">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-              <a class="cat" href="#">
-                Category
-              </a>
-            </div>
-          </div>
-        </div>
-
-        
-        <div id="items-list">
-          {/* Beginning of items box, this will be a loop per category */}
-          <a class="anchor" id="cat1"></a> {/* What the category button links to, anchor provides buffer from header, id is the same as the corresponding button */}
-          <div class="items-grid-cont">
-            <h3>Category</h3>
-            <div class="inner-grid-cont">
-              {/* This will be a loop per item */}
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
+  
+          
+          <div id="items-list">
+            {/* Beginning of items box, this will be a loop per category */}
+            <a class="anchor" id="cat1"></a> {/* What the category button links to, anchor provides buffer from header, id is the same as the corresponding button */}
+            <div class="items-grid-cont">
+              <h3>Category</h3>
+              <div class="inner-grid-cont">
+                {/* This will be a loop per item */}
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
               </div>
             </div>
-          </div>
-
-          {/* For dynamic data remove this section */}
-          <a class="anchor" id="cat2"></a> 
-          <div class="items-grid-cont" id="cat2">
-            <h3>Category</h3>
-            <div class="inner-grid-cont">
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
-              </div>
-              <div class="items-grid-item">
-                <ItemCards></ItemCards>
+  
+            {/* For dynamic data remove this section */}
+            <a class="anchor" id="cat2"></a> 
+            <div class="items-grid-cont" id="cat2">
+              <h3>Category</h3>
+              <div class="inner-grid-cont">
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
+                <div class="items-grid-item">
+                  <ItemCards></ItemCards>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div id="extra-space-store"></div>
       </div>
-      <div id="extra-space-store"></div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Store;
