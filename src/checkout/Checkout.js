84436acd2627632188dcca4f0ae6acf99a1951d6
seaungst <selena.elise03@gmail.com>
2021-05-11 01:09:16 -0700

    last last minute changes

--- src/checkout/Checkout.js
+++ src/checkout/Checkout.js
@@ -1,7 +1,7 @@
-import React, { lazy } from 'react'
-import { Redirect } from 'react-router-dom'
+import React, { lazy } from "react";
+import { Redirect } from "react-router-dom";
 import "./Checkout.css";
-import axios from 'axios'
+import axios from "axios";
 
 /* importing the form components
 notes to self
@@ -14,241 +14,243 @@ const ReviewOrder = lazy(() => import("./ReviewOrder"));
 axios.defaults.withCredentials = true;
 
 export default class Checkout extends React.Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            step: 1,
-            Shipping_Street: "",
-            Shipping_City: "",
-            Shipping_State: "",
-            Shipping_ZIP: "",
-            Handler: {},
-            Delivery_Instructions: "",
-            Card_Number: "",
-            Card_Name: "",
-            CVV: "",
-            Exp_Date: '',
-            Billing_Street: "",
-            Billing_City: "",
-            Billing_State: "",
-            Billing_ZIP: "",
-            order_placed: null
-        }
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this._next = this._next.bind(this);
-        this._prev = this._prev.bind(this);
-    }
+    this.state = {
+      step: 1,
+      Shipping_Street: "",
+      Shipping_City: "",
+      Shipping_State: "",
+      Shipping_ZIP: "",
+      Handler: {},
+      Delivery_Instructions: "",
+      Card_Number: "",
+      Card_Name: "",
+      CVV: "",
+      Exp_Date: "",
+      Billing_Street: "",
+      Billing_City: "",
+      Billing_State: "",
+      Billing_ZIP: "",
+      order_placed: null,
+    };
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this._next = this._next.bind(this);
+    this._prev = this._prev.bind(this);
+  }
 
-    _next() {
-        let step = this.state.step
-        // If the current step is 1 or 2, then add one on "next" button click
-        step = step >= 2? 3: step + 1
-        this.setState({
-          step: step
-        })
-      }
-    
-      _prev() {
-        let step = this.state.step
-        // If the current step is 2 or 3, then subtract one on "previous" button click
-        step = step <= 1? 1: step - 1
-        this.setState({
-          step: step
-        })
-      }
-      // The "next" and "previous" button functions
-get previousButton(){
+  _next() {
+    let step = this.state.step;
+    // If the current step is 1 or 2, then add one on "next" button click
+    step = step >= 2 ? 3 : step + 1;
+    this.setState({
+      step: step,
+    });
+  }
+
+  _prev() {
+    let step = this.state.step;
+    // If the current step is 2 or 3, then subtract one on "previous" button click
+    step = step <= 1 ? 1 : step - 1;
+    this.setState({
+      step: step,
+    });
+  }
+  // The "next" and "previous" button functions
+  get previousButton() {
     let step = this.state.step;
     // If the current step is not 1, then render the "previous" button
-    if(step !==1){
+    if (step !== 1) {
       return (
-        <button 
-        className="btn" 
-        type="button" 
-        onClick={this._prev}>
-            Previous
+        <button className="btn" type="button" onClick={this._prev}>
+          Previous
         </button>
-      )
+      );
     }
     // ...else return nothing
     return null;
   }
-  
-  get nextButton(){
+
+  get nextButton() {
     let step = this.state.step;
     // If the current step is not 3, then render the "next" button
-    if(step <3){
+    if (step < 3) {
       return (
-        <button 
-        className="btn float-right" 
-        type="button" 
-        onClick={this._next}>
+        <button className="btn float-right" type="button" onClick={this._next}>
           Next
-        </button> 
-      )
+        </button>
+      );
     }
     // ...else render nothing
     return null;
   }
 
-    handleChange(event) {
-        const {name, value} = event.target;
-        this.setState({
-            [name]: value
-        })
-    }
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
 
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const { 
-            Shipping_Street, 
-            Shipping_City, 
-            Shipping_State, 
-            Shipping_ZIP,
-            Handler,
-            Delivery_Instructions,
-            Card_Name,
-            Card_Number,
-            CVV,
-            Exp_Date,
-            Billing_Street,
-            Billing_City,
-            Billing_State,
-            Billing_ZIP
-         } = this.state
-         //alert(`${JSON.parse(Handler).Handler_ID}`)
-      }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+      Shipping_Street,
+      Shipping_City,
+      Shipping_State,
+      Shipping_ZIP,
+      Handler,
+      Delivery_Instructions,
+      Card_Name,
+      Card_Number,
+      CVV,
+      Exp_Date,
+      Billing_Street,
+      Billing_City,
+      Billing_State,
+      Billing_ZIP,
+    } = this.state;
+    //alert(`${JSON.parse(Handler).Handler_ID}`)
+  };
 
-    componentDidMount(){
-      let currentComponent = this;
-      axios.get(`https://chickpeaapi.glitch.me/user/account-details`)
-        .then((res) => {
-          if(res.data.payments.length !== 0){
-            const data = res.data;
-            var shipping_addr = {};
-            var billing_addr = {};
-            var payment = {};
-            // getting addresses
-            for(var address of data.addresses){
-              if(address.Is_Billing){
-                billing_addr.Street = address.Street;
-                billing_addr.City = address.City;
-                billing_addr.State = address.State;
-                billing_addr.ZIP = address.Zip_Code;
-              }
-              if(address.Is_Shipping){
-                shipping_addr.Street = address.Street;
-                shipping_addr.City = address.City;
-                shipping_addr.State = address.State;
-                shipping_addr.ZIP = address.Zip_Code;
-              }
+  componentDidMount() {
+    let currentComponent = this;
+    axios
+      .get(`https://chickpeaapi.glitch.me/user/account-details`)
+      .then((res) => {
+        if (res.data.payments.length !== 0) {
+          const data = res.data;
+          var shipping_addr = {};
+          var billing_addr = {};
+          var payment = {};
+          // getting addresses
+          for (var address of data.addresses) {
+            if (address.Is_Billing) {
+              billing_addr.Street = address.Street;
+              billing_addr.City = address.City;
+              billing_addr.State = address.State;
+              billing_addr.ZIP = address.Zip_Code;
             }
-  
-            // getting payment
-            payment.Card_Name = data.payments[0].Card_Name;
-            payment.Card_Number = data.payments[0].Card_Num;
-            payment.CVV = data.payments[0].CVV;
-            payment.Exp_Date = data.payments[0].Exp_Date;
-  
-            // setting the state
-            currentComponent.setState({
-              Shipping_Street: shipping_addr.Street,
-              Shipping_City: shipping_addr.City,
-              Shipping_State: shipping_addr.State,
-              Shipping_ZIP: shipping_addr.ZIP,
-              Card_Number: payment.Card_Number.toString(),
-              Card_Name: payment.Card_Name,
-              CVV: payment.CVV,
-              Exp_Date: payment.Exp_Date,
-              Billing_Street: billing_addr.Street,
-              Billing_City: billing_addr.City,
-              Billing_State: billing_addr.State,
-              Billing_ZIP: billing_addr.ZIP
-            })
+            if (address.Is_Shipping) {
+              shipping_addr.Street = address.Street;
+              shipping_addr.City = address.City;
+              shipping_addr.State = address.State;
+              shipping_addr.ZIP = address.Zip_Code;
+            }
           }
-        })
-    }
 
-    placeOrder(){
-      axios(`https://chickpeaapi.glitch.me/checkout`,{
-        method: 'post',
-        data: {
-          Shipping_Street: this.state.Shipping_Street,
-          Shipping_City: this.state.Shipping_City,
-          Shipping_State: this.state.Shipping_State,
-          Shipping_ZIP: this.state.Shipping_ZIP,
-          Delivery_Instructions: this.state.Delivery_Instructions,
-          Billing_Street: this.state.Billing_Street,
-          Billing_City: this.state.Billing_City,
-          Billing_State: this.state.Billing_State,
-          Billing_ZIP: this.state.Billing_ZIP,
-          Handler_ID: JSON.parse(this.state.Handler).Handler_ID
-        }
-      })
-        .then((res) => {
-          console.log(res);
-          this.setState({order_placed: true});
-        })
-    }
+          // getting payment
+          payment.Card_Name = data.payments[0].Card_Name;
+          payment.Card_Number = data.payments[0].Card_Num;
+          payment.CVV = data.payments[0].CVV;
+          payment.Exp_Date = data.payments[0].Exp_Date;
 
-    render() {
-        if(this.state.order_placed){
-          return(
-            <Redirect to="/order-placed" />
-        );
+          // setting the state
+          currentComponent.setState({
+            Shipping_Street: shipping_addr.Street,
+            Shipping_City: shipping_addr.City,
+            Shipping_State: shipping_addr.State,
+            Shipping_ZIP: shipping_addr.ZIP,
+            Card_Number: payment.Card_Number.toString(),
+            Card_Name: payment.Card_Name,
+            CVV: payment.CVV,
+            Exp_Date: payment.Exp_Date,
+            Billing_Street: billing_addr.Street,
+            Billing_City: billing_addr.City,
+            Billing_State: billing_addr.State,
+            Billing_ZIP: billing_addr.ZIP,
+          });
         }
-        return (
-            <div id="checkout-page">
-                <h1>Checkout</h1>
-                <p>If you have ordered with us before, Chickpea will prefill these fields with your personal information.</p>
-                    <DeliveryDetails
-                        step = {this.state.step}
-                        handleChange = {this.handleChange}
-                        Shipping_Street = {this.state.Shipping_Street}
-                        Shipping_City = {this.state.Shipping_City}
-                        Shipping_State = {this.state.Shipping_State}
-                        Shipping_ZIP = {this.state.Shipping_ZIP}
-                        Delivery_Instructions = {this.state.Delivery_Instructions}
-                        Handler = {this.state.Handler}
-                    />
-                    <PaymentDetails 
-                        step = {this.state.step}
-                        handleChange = {this.handleChange}
-                        Card_Name = {this.state.Card_Name}
-                        Card_Number = {this.state.Card_Number}
-                        CVV = {this.state.CVV}
-                        Exp_Date = {this.state.Exp_Date}
-                        Billing_Street = {this.state.Billing_Street}
-                        Billing_City = {this.state.Billing_City}
-                        Billing_State = {this.state.Billing_State}
-                        Billing_ZIP = {this.state.Billing_ZIP}
-                    />
-                    <ReviewOrder
-                      step = {this.state.step}
-                      Shipping_Street = {this.state.Shipping_Street}
-                      Shipping_City = {this.state.Shipping_City}
-                      Shipping_State = {this.state.Shipping_State}
-                      Shipping_ZIP = {this.state.Shipping_ZIP}
-                      Handler = {this.state.Handler}
-                      Delivery_Instructions = {this.state.Delivery_Instructions}
-                      Card_Name = {this.state.Card_Name}
-                      Card_Number = {this.state.Card_Number}
-                      CVV = {this.state.CVV}
-                      Exp_Date = {this.state.Exp_Date}
-                      Billing_Street = {this.state.Billing_Street}
-                      Billing_City = {this.state.Billing_City}
-                      Billing_State = {this.state.Billing_State}
-                      Billing_ZIP = {this.state.Billing_ZIP}
-                    />
-                    {this.previousButton}
-                    {this.nextButton}
-                    {this.state.step == 3 &&
-                    <div>
-                        <button class="btn float-right" type="submit" onClick={()=>this.placeOrder()}>Place Order</button>
-                    </div>}
-            </div>
-        )
+      });
+  }
+
+  placeOrder() {
+    axios(`https://chickpeaapi.glitch.me/checkout`, {
+      method: "post",
+      data: {
+        Shipping_Street: this.state.Shipping_Street,
+        Shipping_City: this.state.Shipping_City,
+        Shipping_State: this.state.Shipping_State,
+        Shipping_ZIP: this.state.Shipping_ZIP,
+        Delivery_Instructions: this.state.Delivery_Instructions,
+        Billing_Street: this.state.Billing_Street,
+        Billing_City: this.state.Billing_City,
+        Billing_State: this.state.Billing_State,
+        Billing_ZIP: this.state.Billing_ZIP,
+        Handler_ID: JSON.parse(this.state.Handler).Handler_ID,
+      },
+    }).then((res) => {
+      console.log(res);
+      this.setState({ order_placed: true });
+    });
+  }
+
+  render() {
+    if (this.state.order_placed) {
+      return <Redirect to="/order-placed" />;
     }
+    return (
+      <div id="checkout-page">
+        <h1>Checkout</h1>
+        <p>
+          If you have ordered with us before, Chickpea will prefill these fields
+          with your personal information.
+        </p>
+        <DeliveryDetails
+          step={this.state.step}
+          handleChange={this.handleChange}
+          Shipping_Street={this.state.Shipping_Street}
+          Shipping_City={this.state.Shipping_City}
+          Shipping_State={this.state.Shipping_State}
+          Shipping_ZIP={this.state.Shipping_ZIP}
+          Delivery_Instructions={this.state.Delivery_Instructions}
+          Handler={this.state.Handler}
+        />
+        <PaymentDetails
+          step={this.state.step}
+          handleChange={this.handleChange}
+          Card_Name={this.state.Card_Name}
+          Card_Number={this.state.Card_Number}
+          CVV={this.state.CVV}
+          Exp_Date={this.state.Exp_Date}
+          Billing_Street={this.state.Billing_Street}
+          Billing_City={this.state.Billing_City}
+          Billing_State={this.state.Billing_State}
+          Billing_ZIP={this.state.Billing_ZIP}
+        />
+        <ReviewOrder
+          step={this.state.step}
+          Shipping_Street={this.state.Shipping_Street}
+          Shipping_City={this.state.Shipping_City}
+          Shipping_State={this.state.Shipping_State}
+          Shipping_ZIP={this.state.Shipping_ZIP}
+          Handler={this.state.Handler}
+          Delivery_Instructions={this.state.Delivery_Instructions}
+          Card_Name={this.state.Card_Name}
+          Card_Number={this.state.Card_Number}
+          CVV={this.state.CVV}
+          Exp_Date={this.state.Exp_Date}
+          Billing_Street={this.state.Billing_Street}
+          Billing_City={this.state.Billing_City}
+          Billing_State={this.state.Billing_State}
+          Billing_ZIP={this.state.Billing_ZIP}
+        />
+        {this.previousButton}
+        {this.nextButton}
+        {this.state.step == 3 && (
+          <div>
+            <button
+              class="btn float-right"
+              type="submit"
+              onClick={() => this.placeOrder()}
+            >
+              Place Order
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- src/handbasket/HandbasketTable.js
+++ src/handbasket/HandbasketTable.js
@@ -1,96 +1,106 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-export default class HandbasketTable extends React.Component{
-    state = {
-        handbasket: [],
-        total: 0,
-        remove: null
-      };
-    
-      componentDidMount() {
-        axios
-          .get(`https://chickpeaapi.glitch.me/cart`)
-          .then((res) => {
-            const data = res.data;
-            console.log(data);
-            var running_total = 0;
-            for (var item of data) {
-              running_total +=
-               (item.Price.$numberDecimal ? item.Price.$numberDecimal : item.Price) * item.Quantity;
-            }
-            this.setState({ handbasket: data, total: running_total });
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      }
+export default class HandbasketTable extends React.Component {
+  state = {
+    handbasket: [],
+    total: 0,
+    remove: null,
+  };
 
-      removeItem(id){
-        axios(`https://chickpeaapi.glitch.me/cart/remove`, 
-        {
-          method: "post",
-          data: {
-            Item_ID: id
-          }
-        })
-          .then((res) => {
-            console.log(res.data);
-            var new_handbasket = [];
-            var to_remove;
-            for(var item of this.state.handbasket){
-              if(item.Item_ID !== id)
-                new_handbasket.push(item);
-              else
-                to_remove = item;
-            }
-            var new_total = this.state.total - (to_remove.Price.$numberDecimal * to_remove.Quantity);
-            this.setState({ handbasket: new_handbasket, total: new_total });
-          })
-      }
+  componentDidMount() {
+    axios
+      .get(`https://chickpeaapi.glitch.me/cart`)
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+        var running_total = 0;
+        for (var item of data) {
+          running_total +=
+            (item.Price.$numberDecimal
+              ? item.Price.$numberDecimal
+              : item.Price) * item.Quantity;
+        }
+        this.setState({ handbasket: data, total: running_total });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-    render(){
-        return(
-            <div class="handbasket-contents">
-            <table id="handbasket-table">
-                <thead>
-                <tr>
-                    <th id="product-column">Product</th>
-                    <th id="store-column">Store</th>
-                    <th id="price-column">Price</th>
-                    <th id="quantity-column">Quantity</th>
-                    <th id="subtotal-column">Subtotal</th>
-                    <th id="remove"></th>
-                </tr>
-                </thead>
-                {this.state.handbasket.map((item) => (
-                <tr>
-                    <td>
-                    <div class="product-image">
-                        <img
-                        src={item.Image_URL}
-                        width="150px"
-                        height="150px"
-                        ></img>
-                    </div>
-                    <div class="product-name">{item.Item_Name}</div>
-                    </td>
-                    <td>{item.Store_Name}</td>
-                    <td>${item.Price.$numberDecimal ? item.Price.$numberDecimal : item.Price }</td>
-                    <td>{item.Quantity}</td>
-                    <td>${(item.Price.$numberDecimal ? item.Price.$numberDecimal : item.Price) * item.Quantity}</td>
-                    <td>
-                      <button className="btn" onClick={()=>this.removeItem(item.Item_ID)}>Remove from Handbasket</button>
-                    </td>
-                </tr>
-                ))}
-            </table>
-            <div id="total">
-                <b>Total: ${this.state.total.toFixed(2)}</b>
-            </div>
-            </div>
-        );
-    }
+  removeItem(id) {
+    axios(`https://chickpeaapi.glitch.me/cart/remove`, {
+      method: "post",
+      data: {
+        Item_ID: id,
+      },
+    }).then((res) => {
+      console.log(res.data);
+      var new_handbasket = [];
+      var to_remove;
+      for (var item of this.state.handbasket) {
+        if (item.Item_ID !== id) new_handbasket.push(item);
+        else to_remove = item;
+      }
+      var new_total =
+        this.state.total - to_remove.Price.$numberDecimal * to_remove.Quantity;
+      this.setState({ handbasket: new_handbasket, total: new_total });
+    });
+  }
+
+  render() {
+    return (
+      <div class="handbasket-contents">
+        <table id="handbasket-table">
+          <thead>
+            <tr>
+              <th id="product-column">Product</th>
+              <th id="store-column">Store</th>
+              <th id="price-column">Price</th>
+              <th id="quantity-column">Quantity</th>
+              <th id="subtotal-column">Subtotal</th>
+              <th id="remove"></th>
+            </tr>
+          </thead>
+          {this.state.handbasket.map((item) => (
+            <tr>
+              <td>
+                <div class="product-image">
+                  <img src={item.Image_URL} width="150px" height="150px"></img>
+                </div>
+                <div class="product-name">{item.Item_Name}</div>
+              </td>
+              <td>{item.Store_Name}</td>
+              <td>
+                $
+                {item.Price.$numberDecimal
+                  ? item.Price.$numberDecimal
+                  : item.Price}
+              </td>
+              <td>{item.Quantity}</td>
+              <td>
+                $
+                {(item.Price.$numberDecimal
+                  ? item.Price.$numberDecimal
+                  : item.Price) * item.Quantity}
+              </td>
+              <td>
+                <button
+                  className="btn"
+                  onClick={() => this.removeItem(item.Item_ID)}
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </table>
+        <div id="total">
+          <b>Total: ${this.state.total.toFixed(2)}</b>
+        </div>
+      </div>
+    );
+  }
 }
