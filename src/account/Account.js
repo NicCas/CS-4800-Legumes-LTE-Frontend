1b7f5796bb1ce7bad702df1598242d53c7090f3e
Ryan <ryan.atienza97@gmail.com>
2021-05-10 23:40:27 -0700

    adjust account page

--- src/account/Account.js
+++ src/account/Account.js
@@ -89,6 +89,109 @@ export default class Account extends React.Component {
   }
 
   render() {
+    if(!this.state.transporting.Delivery_ID){
+      return (
+        <div id="account-page">
+          <div id="account-header" class="account-wrapper">
+            <div id="name-image">
+              <img src={account} class="account-image"></img>
+              <h3 class="account-title">Hello, {this.state.customer.Username}</h3>
+            </div>
+            <div id="logout-button">
+              <a href="/home">
+                <button
+                  href="/home"
+                  class="purple-button btn"
+                  onClick={() => this.handleLogout()}
+                >
+                  Logout
+                </button>
+              </a>
+            </div>
+          </div>
+          <div class="account-grid-container">
+            <div id="left-column">
+              <div id="profile-info" class="account-grid-item">
+                <h3>Account Information</h3>
+                <div class="info-block" id="info-title">
+                  <p>Username </p>
+                  <p>Email </p>
+                  <p>Billing Address </p>
+                  <p>Shipping Address </p>
+                </div>
+                <div class="info-block">
+                  <p> {this.state.customer.Username} </p>
+                  <p> {this.state.customer.Email} </p>
+                  {this.state.billings &&
+                    <p>
+                      {this.state.billings.Street}, {this.state.billings.City},{" "}
+                      {this.state.billings.State}, {this.state.billings.Zip_Code}{" "}
+                    </p>
+                  }
+                  {
+                    !this.state.billings &&
+                    <p>none on file</p>
+                  }
+                  {this.state.shippings &&
+                    <p>
+                      {this.state.shippings.Street}, {this.state.shippings.City},{" "}
+                      {this.state.shippings.State}, {this.state.shippings.Zip_Code}
+                    </p>
+                  }
+                  {
+                    !this.state.shippings &&
+                    <p>none on file</p>
+                  }
+                </div>
+              </div>
+              <div id="favorites" class="account-grid-item">
+                <h3>Favorites</h3>
+                <table>
+                  <tr>
+                    <th class="item-column">Item</th>
+                    <th class="store-column">Store</th>
+                    <th class="price-column">Price</th>
+                  </tr>
+  
+                  {this.state.favs.map((favorites) => (
+                    <tr>
+                      <td>{favorites.Item_Name}</td>
+                      <td> {favorites.Store_ID} </td>
+                      <td>${favorites.Price.toFixed(2)}</td>
+                    </tr>
+                  ))}
+                </table>
+              </div>
+            </div>
+          </div>
+          <div id="order-history" class="account-grid-item">
+            <h3>Order History</h3>
+            <table>
+              <tr>
+                <th>Order Number</th>
+                <th>Date</th>
+                <th>Total Cost</th>
+                <th>Delivery Address</th>
+                <th>Items</th>
+              </tr>
+              {this.state.delivs.map((deliveries) => (
+                <tr>
+                  <td>{deliveries.Delivery_ID}</td>
+                  <td> {deliveries.Date} </td>
+                  <td>${deliveries.Total_Cost}</td>
+                  <td>
+                    {this.state.shippings.Street},{this.state.shippings.City},
+                    {this.state.shippings.State},{this.state.shippings.Zip_Code}
+                  </td>
+                  <td>this.state.deliv_items</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+          <div id="extra-space-account"></div>
+        </div>
+      );
+    }
     return (
       <div id="account-page">
         <div id="account-header" class="account-wrapper">
@@ -162,14 +265,12 @@ export default class Account extends React.Component {
               </table>
             </div>
           </div>
-          {this.state.transporting.length > 0 &&
             <div id="recent-order" class="account-grid-item">
             <CurrentOrder
               transporting={this.state.transporting}
               shippings={this.state.shippings}
             />
             </div>
-          }
         </div>
         <div id="order-history" class="account-grid-item">
           <h3>Order History</h3>
